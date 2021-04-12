04a2e1aabc92bee6c3344076f03afa3261e751c0
Костя Столярский <73879155+StoliarskyiKostiantyn@users.noreply.github.com>
2021-04-12 19:18:30 +0300

    Render of markup

--- js-hw-08.js
+++ js-hw-08.js
@@ -1,2 +1,9 @@
 import galleryItems from "./gallery-items.js";
-console.log(galleryItems);
+const gallery_list = document.querySelector(".js-gallery");
+const makeGallaryItemMarkup = ({ preview, description }) => {
+  return `
+    <li class="gallery__item"><a class="gallery__link"><img  class="gallery__image" src="${preview}" alt="${description}"></a></li>
+    `;
+};
+const makeGallaryMarkup = galleryItems.map(makeGallaryItemMarkup).join("");
+gallery_list.insertAdjacentHTML("afterbegin", makeGallaryMarkup);
