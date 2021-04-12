be9676a2dc97d5aa38955556f5256bb7c8cd7a80
Костя Столярский <73879155+StoliarskyiKostiantyn@users.noreply.github.com>
2021-04-12 19:56:20 +0300

    Maling click on img

--- js-hw-08.js
+++ js-hw-08.js
@@ -1,5 +1,7 @@
 import galleryItems from "./gallery-items.js";
 const gallery_list = document.querySelector(".js-gallery");
+const lightboxEl = document.querySelector(".js-lightbox");
+const ligthboxImg = document.querySelector(".lightbox__image");
 const makeGallaryItemMarkup = ({ preview, description }) => {
   return `
     <li class="gallery__item"><a class="gallery__link"><img  class="gallery__image" src="${preview}" alt="${description}"></a></li>
@@ -7,3 +9,12 @@ const makeGallaryItemMarkup = ({ preview, description }) => {
 };
 const makeGallaryMarkup = galleryItems.map(makeGallaryItemMarkup).join("");
 gallery_list.insertAdjacentHTML("afterbegin", makeGallaryMarkup);
+gallery_list.addEventListener("click", onImgClick);
+function onImgClick(evt) {
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  lightboxEl.classList.add("is-open");
+  ligthboxImg.src = evt.target.src;
+  console.log(evt.target.nodeName);
+}
